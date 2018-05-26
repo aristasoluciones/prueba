68683f85d900f73c793417c656655cc195d74c98
Arista Soluciones <isc061990@outlook.com>
2018-05-25 22:19:33 -0500

    dd

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
  
         include_once('config.php');
-        /*$conId = mysql_connect($sqlHost, $sqlUser, $sqlPw, 1);
+        $conId = mysql_connect($sqlHost, $sqlUser, $sqlPw, 1);
 		mysql_set_charset('utf8');
-    	mysql_select_db($sqlDb, $this->$conId) or die("<br/>".mysql_error()."<br/>");*/
+    	mysql_select_db($sqlDb, $this->$conId) or die("<br/>".mysql_error()."<br/>");
    
 ?>
