c1f48d0e4ca1370b5dfeb1a987d43a2232dc17cf
Arista Soluciones <isc061990@outlook.com>
2018-05-25 22:14:20 -0500

    correccion 2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo phpinfo(); 
-      /* include('config.php');
+ 
+        include_once('config.php');
         $conId = mysql_connect($sqlHost, $sqlUser, $sqlPw, 1);
 		mysql_set_charset('utf8');
     	mysql_select_db($sqlDb, $this->$conId) or die("<br/>".mysql_error()."<br/>");*/
