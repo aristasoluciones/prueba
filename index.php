c81cb04a64ea4a3bc056737e6154b7d559b11a70
Arista Soluciones <isc061990@outlook.com>
2018-05-27 22:30:19 -0500

    isis

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
         ini_set("display_errors", 1); 
- 
+ echo php_info();
         include_once('config.php');
         $conId = mysqli($sqlHost, $sqlUser, $sqlPw, 1);
 		mysql_set_charset('utf8');
