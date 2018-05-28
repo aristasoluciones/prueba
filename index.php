534d9122ccc95f7ad0600ffb47304707bc58bdc7
Arista Soluciones <isc061990@outlook.com>
2018-05-27 21:04:53 -0500

    correccion 6

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
         ini_set("display_errors", 1); 
  
         include_once('config.php');
-        $conId = mysql_connect($sqlHost, $sqlUser, $sqlPw, 1);
+        $conId = mysqli($sqlHost, $sqlUser, $sqlPw, 1);
 		mysql_set_charset('utf8');
     	mysql_select_db($sqlDb, $this->$conId) or die("<br/>".mysql_error()."<br/>");
    
