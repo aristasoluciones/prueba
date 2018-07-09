9f0e3709b60b4acd181bb079e1a85b997570420f
Arista Soluciones <isc061990@outlook.com>
2018-07-09 15:56:21 -0500

    ggg

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $region ="nyc3";
                 false // False since we're not using flash
             );
 
-$uploadURL = 'https://' . $bucket . '.s3.amazonaws.com/';
+$uploadURL = 'https://' . $bucket . '.nyc3.digitaloceanspaces.com/';
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
