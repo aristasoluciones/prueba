bbed8ea036efbb94ee08c42b6208f398b23f454a
Arista Soluciones <isc061990@outlook.com>
2018-07-08 21:15:04 -0500

    dd

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <?php
-        ini_set("display_errors", 1); 
- echo phpinfo();
+
         include_once('config.php');
-        $conId = mysqli($sqlHost, $sqlUser, $sqlPw, 1);
-		mysql_set_charset('utf8');
-    	mysql_select_db($sqlDb, $this->$conId) or die("<br/>".mysql_error()."<br/>");
-   
+$message = '';
+if(!empty($_FILES['uploaded_file'])) {
+  $path = $rutaDocumento;
+  $path = $path . uniqid('u', true) . '-' . basename($_FILES['uploaded_file']['name']);
+
+  if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+    $message = 'The file ' . basename( $_FILES['uploaded_file']['name']) . ' has been uploaded';
+  } else {
+    $message = 'There was an error uploading the file, please try again!';
+    $message .= $_FILES['uploaded_file']['error'];
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <?php echo $message; ?>
+  <form enctype="multipart/form-data" action="index.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
+
 ?>
